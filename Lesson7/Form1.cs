e5ba9457332988298de4d7c91d70051919c01d2d
Uio443 <ronaldkirk101pc@gmail.com>
2018-10-28 21:12:34 -0400

    Lesson 7 Complete (Part 1)

---
+++ Lesson7/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Lesson7;
+
+namespace Lesson7
+{
+    public partial class fMain : Form
+    {
+        Udvoitel udvoitel;
+
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            udvoitel = new Udvoitel(10, 100);
+            UpdateInfo();
+        }
+
+        private void UpdateInfo()
+        {
+            lblFinish.Text = udvoitel.Finish.ToString();
+            tbCurrent.Text = udvoitel.Current.ToString();
+            lblTurns.Text = udvoitel.Turns.ToString();
+            lblMinTurns.Text = udvoitel.MinTurns.ToString();
+            
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            if (udvoitel == null)
+            {
+                MessageBox.Show("Start game!");
+                return;
+            }
+            udvoitel.Plus();
+            UpdateInfo();
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            if (udvoitel == null)
+            {
+                MessageBox.Show("Start game!");
+                return;
+            }
+            udvoitel.Multi();
+            UpdateInfo();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            if (udvoitel == null)
+            {
+                MessageBox.Show("Start game!");
+                return;
+            }
+            udvoitel.Reset();
+            UpdateInfo();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (udvoitel == null)
+            {
+                MessageBox.Show("Start game!");
+                return;
+            }
+            if (udvoitel.Turns != 0) {
+                udvoitel.Back();
+                UpdateInfo();
+
+            }
+
+        }
+    }
+}
---
+++ Lesson7/Udvoitel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson7
+{
+    class Udvoitel
+    {
+        public int finish, current, turns, minTurns, turnsA;
+        public List<int> turnsArray = new List<int>();
+
+        public Udvoitel(int min,int max)
+        {
+            Random rnd = new Random();
+            this.finish = rnd.Next(min,max+1);
+            minTurnsCalc(finish);
+            current = 1;
+            //turnsArray.Add(1);
+        }
+
+        public void minTurnsCalc(int fin) {
+            while (fin != 1) {
+                minTurns++;
+                if (fin % 2 == 0)
+                {
+                    fin = fin / 2;
+                }
+                else if (fin % 2 == 1) {
+                    fin--;
+                }
+            }
+        }
+
+        public void Plus()
+        {
+            current++;
+            turnsArray.Add(current);
+            turns++;
+        }
+
+        public void Multi()
+        {
+            current *= 2;
+            turnsArray.Add(current);
+            turns++;
+        }
+
+        public void Reset()
+        {
+            current = 1;
+            turnsArray.Clear();
+            turns = 0;
+        }
+
+        public void Back()
+        {
+            if (turns != 1)
+            {
+                turns--;
+                current = turnsArray[turns - 1];
+            }
+            else {
+                current = 1;
+                turns = 0;
+            }
+        }
+
+        public int Current
+        {
+            get { return current; }
+        }
+
+        public int Finish
+        {
+            get { return finish; }
+        }
+
+        public int Turns
+        {
+            get { return turns; }
+        }
+
+        public int MinTurns
+        {
+            get { return minTurns; }
+        }
+        //1=>25 +1 x2
+    }
+}
